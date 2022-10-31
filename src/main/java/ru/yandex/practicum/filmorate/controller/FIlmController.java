21a74e88e05c9e4169ae113cca9963732c4e2cbc
RufatYagofarov <rufat.yagofarov@yandex.ru>
2022-10-31 15:55:34 +0300

    Выполнить замечания ревьюера #1

--- src/main/java/ru/yandex/practicum/filmorate/controller/FIlmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FIlmController.java
@@ -2,10 +2,10 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import javax.validation.Valid;
-import javax.validation.ValidationException;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
@@ -22,64 +22,88 @@ public class FIlmController {
         return ++id;
     }
 
+
     @GetMapping
     public Collection<Film> findAll() {
+        log.info("Выводим список всех фильмов, размер списка: '{}'", films.size());
         return films.values();
     }
 
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
 
-        if (films.containsKey(film.getId())) {
-            log.info("Id фильма '{}' ", film.getId());
-            throw new ValidationException("Фильм с таким id уже существует!");
-        }
+        log.info("Проверяем film в валидаторах");
+        validateExistenceForPOST(film);
+        validateDescription(film);
+        validateReleaseDate(film);
 
-        if (film.getDescription().length() > 200) {
-            log.info("Размер описания '{}' ", film.getDescription().length());
-            throw new ValidationException("Длина описания не может превышать 200 символов!");
-        }
-
-        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            log.info("Дата релиза '{}' ", film.getReleaseDate());
-            throw new ValidationException("Дата релиза не должна быть раньше 28 декабря 1895!");
-        }
-
+        log.info("Присваиваем id");
         film.setId(makeID());
-        films.put(film.getId(), film);
-        return film;
+
+        log.info("Создаем объект в билдере");
+        Film filmFromCreator = filmCreator(film);
+
+        log.info("Добавляем объект в коллекцию");
+        films.put(filmFromCreator.getId(), filmFromCreator);
+
+        return filmFromCreator;
     }
 
     @PutMapping
     public Film put(@Valid @RequestBody Film film) {
 
-        if (!films.containsKey(film.getId())) {
-            log.info("Id фильма '{}' ", film.getId());
-            throw new ValidationException("Фильм с таким id осутствует!");
-        }
+        log.info("Проверяем film в валидаторах");
+        validateExistenceForPUT(film);
+        validateDescription(film);
+        validateReleaseDate(film);
 
-        if (film.getDescription().length() > 200) {
-            log.info("Размер описания '{}' ", film.getDescription().length());
-            throw new ValidationException("Длина описания не может превышать 200 символов!");
-        }
+        log.info("Создаем объект в билдере");
+        Film filmFromCreator = filmCreator(film);
 
-        if (film.getReleaseDate().isBefore(LocalDate.of(1985, 12, 28))) {
-            log.info("Дата релиза '{}' ", film.getReleaseDate());
-            throw new ValidationException("Дата релиза не должна быть раньше 28 декабря 1895!");
-        }
+        log.info("Добавляем объект в коллекцию");
+        films.put(filmFromCreator.getId(), filmFromCreator);
 
-        log.info("Создаем объект в билдере '{}'", film.getName());
-        Film filmBuilder = Film.builder()
+        return filmFromCreator;
+    }
+
+    public Film filmCreator(Film film) {
+        Film filmFromBuilder = Film.builder()
                 .id(film.getId())
                 .name(film.getName())
                 .description(film.getDescription())
                 .releaseDate(film.getReleaseDate())
                 .duration(film.getDuration())
                 .build();
+        log.info("Объект Film создан '{}'", filmFromBuilder.getName());
+        return filmFromBuilder;
+    }
 
-        films.put(filmBuilder.getId(), filmBuilder);
+    public void validateExistenceForPOST(Film film) {
+        if (films.containsKey(film.getId())) {
+            log.info("Id фильма '{}' ", film.getId());
+            throw new ValidationException("Фильм с таким id уже существует!");
+        }
+    }
 
-        return filmBuilder;
+    public void validateExistenceForPUT(Film film) {
+        if (!films.containsKey(film.getId())) {
+            log.info("Id фильма '{}' ", film.getId());
+            throw new ValidationException("Фильм с таким id осутствует!");
+        }
+    }
+
+    public void validateDescription(Film film) {
+        if (film.getDescription().length() > 200) {
+            log.info("Размер описания '{}' ", film.getDescription().length());
+            throw new ValidationException("Длина описания не может превышать 200 символов!");
+        }
+    }
+
+    public void validateReleaseDate(Film film) {
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.info("Дата релиза '{}' ", film.getReleaseDate());
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895!");
+        }
     }
 }
 
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,10 +2,10 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
-import javax.validation.ValidationException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,49 +15,91 @@ import java.util.Map;
 @RequestMapping("/users")
 public class UserController {
     private final Map<Integer, User> users = new HashMap<>();
-    private int userId = 0;
+    private int Id = 0;
 
     private int makeID() {
-        return ++userId;
+        return ++Id;
     }
 
     @GetMapping
     public Collection<User> findAll() {
+        log.info("Выводим список всех пользователей, размер списка: '{}'", users.size());
         return users.values();
     }
 
     @PostMapping
-    public User create(@Valid @RequestBody User user) throws ValidationException {
+    public User create(@Valid @RequestBody User user) {
 
-        if (users.containsKey(user.getId()))
-            throw new ValidationException("Пользователь с таким id уже существует!");
+        log.info("Проверяем user в валидаторах");
+        validateExistenceForPOST(user);
+        validateLogin(user);
+        user = validateName(user);
 
-        if (user.getLogin().contains(" "))
-            throw new ValidationException("Пробел в login недопустим!");
-
-        if (user.getName() == null || user.getName().isBlank())
-            user.setName(user.getLogin());
-
-        log.info("Создаем объект");
+        log.info("Присваиваем id");
         user.setId(makeID());
-        users.put(user.getId(), user);
-        return user;
+
+        User userFromCreator = userCreator(user);
+
+        log.info("Добавляем объект в коллекцию");
+        users.put(userFromCreator.getId(), userFromCreator);
+
+        return userFromCreator;
     }
 
     @PutMapping
-    public User put(@Valid @RequestBody User user) throws ValidationException {
+    public User put(@Valid @RequestBody User user) {
 
-        if (!users.containsKey(user.getId()))
-            throw new ValidationException("Пользователь отсутствует!");
+        log.info("Проверяем user в валидаторах");
+        validateExistenceForPUT(user);
+        validateLogin(user);
+        user = validateName(user);
 
-        if (user.getLogin().contains(" "))
-            throw new ValidationException("Пробел в login недопустим!");
+        User userFromCreator = userCreator(user);
 
-        if (user.getName() == null || user.getName().isBlank())
-            user.setName(user.getLogin());
+        log.info("Обновляем объект в коллекции");
+        users.put(userFromCreator.getId(), userFromCreator);
 
+        return user;
+    }
+
+    public User userCreator(User user) {
         log.info("Создаем объект");
-        users.put(user.getId(), user);
+        User userFromBuilder = User.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .login(user.getLogin())
+                .name(user.getName())
+                .birthday(user.getBirthday())
+                .build();
+        log.info("Объект User создан, имя : '{}'", userFromBuilder.getName());
+        return userFromBuilder;
+    }
+
+    public void validateExistenceForPOST(User user) {
+        if (users.containsKey(user.getId())) {
+            log.info("Id пользователя '{}' ", user.getId());
+            throw new ValidationException("Пользователь с таким id уже существует!");
+        }
+    }
+
+    public void validateExistenceForPUT(User user) {
+        if (!users.containsKey(user.getId())) {
+            log.info("Id пользователя '{}' ", user.getId());
+            throw new ValidationException("Пользователь отсутствует!");
+        }
+    }
+
+    public void validateLogin(User user) {
+        if (user.getLogin().contains(" ")) {
+            log.info("login пользователя '{}' ", user.getLogin());
+            throw new ValidationException("Пробел в login недопустим!");
+        }
+    }
+    public User validateName(User user) {
+        if (user.getName() == null || user.getName().isBlank()) {
+            log.info("Присваиваем поле login '{}' для поля name '{}' ", user.getLogin(), user.getName());
+            user.setName(user.getLogin());
+        }
         return user;
     }
 }
