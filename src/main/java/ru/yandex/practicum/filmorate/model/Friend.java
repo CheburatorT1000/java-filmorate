af90ab5bb8d50ae0bbc2214f683b250255d24ac0
sarcazmatic <sarcazmatic@yandex.ru>
2022-11-19 19:35:22 +0300

    feat: добавлена функциональность ставить лайки и добавлять друзей; добавлен класс-реестр для обработки ошибок; добавлен класс Friend на основе User для хранения друзей; добавлен класс ErrorResponse для возвращения информации об ошибках

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Friend.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+
+import javax.validation.constraints.Email;
+import java.time.LocalDate;
+
+
+@Data
+@Builder(toBuilder = true)
+public class Friend {
+
+    @EqualsAndHashCode.Exclude
+    private int id;
+    @Email
+    @NonNull
+    private String email;
+    @NonNull
+    private String login;
+    private String name;
+    @NonNull
+    private LocalDate birthday;
+
+}
